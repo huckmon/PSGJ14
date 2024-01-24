f37475a6e7493d84dc3590de762424522ede4375
Huckmon <45248261+huckmon@users.noreply.github.com>
2024-01-24 11:46:48 +1030

    swarm speed changed and level 2 completed
    changed level based on feedback

--- Objects/swarm_large.gd
+++ Objects/swarm_large.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 func swarm_char():
 	pass
 
-var move_speed : float = 30
+var move_speed : float = 80
 
 func _physics_process(_delta):
 	velocity = Vector2(1, 0) * move_speed
